8c5b4599954a537d90edef3174dec0854b21c20a
Kahepa <harveykahepa@gmail.com>
2022-06-06 21:04:09 -0700

    Initial commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - entry point
- *   * @void: no arguments
- *    * Description: the program starts and ends here.
- *     * Return: value 0 to exit
- *      */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
