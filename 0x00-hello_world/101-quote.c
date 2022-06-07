77ecbefdf00cd86f75fe84deb0f5796b2db1141e
Kahepa <harveykahepa@gmail.com>
2022-06-06 21:09:15 -0700

    Initial commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
  *  * main - entry point
  *   * Description: the program writes to stderr.
  *    * Return: value 1 on success.
- *     */
+ *     *
+ *      */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
